6a149fd0400270dc165283c408eb2b8af08e2ac6
Vanja Vrsaljko <vanjavrsaljko@gmail.com>
2015-10-05 17:48:57 +0200

    FT checks for input on a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -18,16 +19,28 @@ class NewVisitorTest(unittest.TestCase):
 
 		#He notices page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		#He is invited to enter a to-do item straight away
-
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+				inputbox.get_attribute('placeholder'),
+				'Enter a to-do item'
+		)
 		#He enters "Buy peacock feathers", his hobby is fly fishing 
 		#lures
-
+		inputbox.send_keys('Buy peacock feathers')
+		
 		#When he hits enter, page updates and the page now lists
 		#"1: Buy peacock feathers" as an item in a to-do list
-
+		inputbox.send_keys(Keys.ENTER)
+		
+		table = self.browser.find.element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
 		#The text box inviting him to add another item is still there.
 		#He enters "Use peacock feathers to make a fly"
 
