5181db702a10178ab5999c69a9b547e0ef93dbe5
Vanja Vrsaljko <vanjavrsaljko@gmail.com>
2015-10-06 16:32:00 +0200

    Added few elements to html homepage

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 class HomePageTest(TestCase):
